98f70811f297ead6a6327101c0ccdfa04abbcba5
Vipul Kale <kalevipul509@gmail.com>
2026-03-17 08:53:33 +0530

    Added LinkedList program

---
+++ LinkedList/CreateLinkedList.java
@@ -0,0 +1,15 @@
+package JAP82.Collection.LinkedList;
+
+import java.util.LinkedList;
+
+public class CreateLinkedList {
+    public static void main(String[] args) {
+        LinkedList<String> l1 = new LinkedList<>();
+        l1.add("vishal");
+        l1.add("vipul");
+        l1.add("yogesh");
+        l1.add("anurag");
+        l1.add("manish");
+        System.out.println(l1);
+    }
+}
