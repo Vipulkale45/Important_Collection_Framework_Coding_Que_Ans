dc6cdc7f3a9f85797c6679bb537e0437ccdcb6a7
Vipul Kale <kalevipul509@gmail.com>
2026-03-18 13:59:39 +0530

    Added some more

---
+++ VectorQue/Traverse.java
@@ -0,0 +1,19 @@
+package JAP82.Collection.VectorQue;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Traverse {
+    public static void main(String[] args) {
+        Vector<String> v = new Vector<>();
+        v.add("java");
+        v.add("Python");
+        v.add("c++");
+
+        Enumeration<String> e = v.elements();
+        while(e.hasMoreElements()){
+            System.out.println(e.nextElement());
+        }
+
+    }
+}
