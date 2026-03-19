af1470fad42fd5d9fe36025fde4f7d4353a85739
Vipul Kale <kalevipul509@gmail.com>
2026-03-19 14:29:51 +0530

    Problems on TreeSet

---
+++ TreeSet/FirstAndLastElement.java
@@ -0,0 +1,11 @@
+package JAP82.Collection.TreeSet;
+
+import java.util.*;
+
+public class FirstAndLastElement {
+    public static void main(String[] args) {
+        TreeSet<Integer> set = new TreeSet<>(Arrays.asList(10,20,30))
+        System.out.println(set.first());
+        System.out.println(set.last());
+    }
+}
---
+++ TreeSet/StoreElement.java
@@ -0,0 +1,10 @@
+package JAP82.Collection.TreeSet;
+
+import java.util.*;
+
+public class StoreElement {
+    public static void main(String[] args) {
+        TreeSet<Integer> set = new TreeSet<>(Arrays.asList(3,1,2));
+        System.out.println(set);
+    }
+}
