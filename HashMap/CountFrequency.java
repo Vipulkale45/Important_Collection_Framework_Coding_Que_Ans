82135ad6341f6996b098651fdc88b55ec7c158fd
Vipul Kale <kalevipul509@gmail.com>
2026-03-20 23:17:35 +0530

    Added another

---
+++ HashMap/CountFrequency.java
@@ -0,0 +1,14 @@
+package JAP82.Collection.HashMap;
+
+import java.util.HashMap;
+
+public class CountFrequency {
+    public static void main(String[] args) {
+        String str = "programming";
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c: str.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+        System.out.println(map);
+    }
+}
